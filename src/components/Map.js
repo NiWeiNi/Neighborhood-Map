172fb5ad3a938c6c1f6ae9a281a5ba4327719ba7
Wei <weinini007@gmail.com>
2018-07-01 19:40:24 +0200

    feat: add infowindow

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Import react-google-maps and its utilities
-import {withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import {withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 // Import styles and personalized marker
 import { mapStyle } from './mapStyle.js';
@@ -14,13 +14,21 @@ const Map = withScriptjs(withGoogleMap(props => {
             defaultZoom={13}
             defaultOptions={{styles: mapStyle.styles}}
         >
-        {props.dataPlaces.map(place => (
-            <Marker
-                position={place.location}
-                key={place.id}
-            >
-            </Marker>
-        ))}
+            {props.dataPlaces.map(place => (
+                <Marker
+                    position={place.location}
+                    key={place.id}
+                >
+                    {<InfoWindow>
+                        <div className="infowindow">
+                            <div className="place-name">{place.name}</div>
+                            <div className="place-direction"></div>
+                            <div className="place-image"></div>
+                        </div>
+                    </InfoWindow>}
+
+                </Marker>
+            ))}
 
         </GoogleMap>
     )}
