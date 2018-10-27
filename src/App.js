c6aebd59efa0320ff4461f20d0e51686f63b5b70
Wei <weinini007@gmail.com>
2018-10-27 17:39:45 +1100

    chore: change Maps API key

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ class App extends Component {
                 </section>
               :
                 <Map
-                  googleMapURL= "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                  googleMapURL= "https://maps.googleapis.com/maps/api/js?key=AIzaSyBfp9jAv6lW9nrntbU48h0yiFS8Kiu8hl4&v=3.exp&libraries=geometry,drawing,places"
                   loadingElement={<div style={{ height: '100%' }} />}
                   containerElement={<div style={{ height: 'calc(100vh - 80px)'}} />}
                   mapElement={<div style={{ height: '100%' }} />}
